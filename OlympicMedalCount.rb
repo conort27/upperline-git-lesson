c10ef3f319e30f9ad4cdb48a0609c67806c2f95b
conort27 <tiernt@d-e.org>
2016-08-09 14:17:13 +0000

    changed Olympic Medal File

---
+++ OlympicMedalCount.rb
@@ -0,0 +1,18 @@
+puts "U.S. OLYMPIC MEDAL COUNT"
+
+num_gold = 5
+num_silv = 7
+num_bronze = 7
+
+total = num_gold + num_silv + num_bronze
+
+puts "The U.S. has won:"
+puts "Gold: #{num_gold}"
+puts "Silver: #{num_silv}"
+puts "Bronze: #{num_bronze}"
+
+puts "swimming".capitalize
+puts "volleyball".upcase
+puts "target shooting".downcase
+puts "air rifle".reverse
+puts "diving".gsub("i", "p")
